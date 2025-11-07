bbacadf840bb13d4dd9d63698e1e0532a00bf2dd
Yere7 <1487987@senati.pe>
2025-11-06 21:01:04 -0500

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,20 +2,16 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Dashboard from './components/Dashboard'; 
-import AlertaStock from './components/AlertaStock'; 
-import './css/Dashboard.css'; // Importamos el CSS tradicional
+import './css/Dashboard.css';
 
 const App: React.FC = () => {
   return (
     <BrowserRouter>
       <Routes>
-        {/* Ruta principal: Muestra el Dashboard (con el Sidebar) */}
+        {  }
         <Route path="/" element={<Dashboard />} /> 
-        
-        {/* Nueva ruta: Muestra SOLO la Alerta de Stock en su propia página */}
-        <Route path="/alertastock" element={<AlertaStock />} />
-        
-        {/* Puedes añadir más rutas aquí: /usuarios, /reportes, etc. */}
+
+        {  }
       </Routes>
     </BrowserRouter>
   );
