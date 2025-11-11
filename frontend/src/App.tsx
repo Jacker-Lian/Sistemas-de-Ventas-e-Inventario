ad96a0955ce0fadbc92a6211390d15878d7a1962
JulioVdE1 <lizandrojulio5@gmail.com>
2025-11-10 19:05:22 -0500

    actualizacion con frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,6 +5,7 @@ import Admin from "./components/Admin";
 import Caja from "./components/Caja";
 import AjusteInventario from './components/AjusteInventario/AjusteInventario'; 
 import PrivateRoute from "./components/PrivateRoute";
+import CrudCategorias from "./components/CategoriaTable";
 
 function App() {
   return (
@@ -34,6 +35,14 @@ function App() {
             element={
               <PrivateRoute roles={['ADMIN', 'CAJA']}>
                 <AjusteInventario />
+              </PrivateRoute>
+            } 
+          />
+          <Route 
+            path="/categorias" 
+            element={
+              <PrivateRoute roles={['ADMIN']}>
+                <CrudCategorias />
               </PrivateRoute>
             } 
           />
--- frontend/src/components/Admin.tsx
+++ frontend/src/components/Admin.tsx
@@ -19,6 +19,7 @@ function Admin() {
     { title: "Productos", desc: "Gestionar productos e inventario", to: "/admin/productos" },
     { title: "Ventas", desc: "Revisar y gestionar ventas", to: "/admin/ventas" },
     { title: "Ajustes", desc: "Ajustes de inventario y movimientos", to: "/admin/ajustes" },
+    { title: "categorías", desc: "Gestionar categorías de productos", to: "/categorias" },
   ];
 
   return (
