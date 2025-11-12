66ac73ab5b3678e63c5224f91464167fe82b481b
WillianJC <willianjoelcandia@gmail.com>
2025-11-11 21:18:03 -0500

    feat(motivoCancelacion): implementar controlador y modelo para gestionar motivos de cancelación

--- backend/src/routes/ventasRoutes.js
+++ backend/src/routes/ventasRoutes.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const ventasController = require("../controllers/ventasController");
+const motivoCancelacionController = require("../controllers/motivoCancelacionController");
+const verificarToken = require("../middleware/verificarToken");
 
 class VentasRoutes {
   constructor() {
@@ -9,17 +11,22 @@ class VentasRoutes {
 
   configurarRutas() {
 
+    // Aplicar middleware de autenticación a todas las rutas
+    this.router.use(verificarToken);
+
+    // Rutas de para gestion de ventas
     this.router.post('/registrar', ventasController.registrarVenta);
-
     this.router.put('/cancelar', ventasController.cancelarVenta);
-
-    this.router.post('/Insertar-motivo-cancelacion', ventasController.registrarMotivoCancelacion);
-
-    this.router.get('/Obtener-motivos-cancelacion', ventasController.obtenerMotivosCancelacion);
-
-    this.router.put('/Desactivar-motivo-cancelacion', ventasController.desactivarMotivoCancelacion);
-
     this.router.put('/Desactivar-ventas', ventasController.desactivarVentas);
+
+    // Rutas para gestion de motivos de cancelación(Relacionado con ventas)
+    this.router.post('/Insertar-motivo-cancelacion', motivoCancelacionController.register);
+    this.router.get('/Obtener-motivos-cancelacion', motivoCancelacionController.getAll);
+    this.router.put('/Desactivar-motivo-cancelacion', motivoCancelacionController.deactive);
+    this.router.get('/Obtener-motivo-cancelacion-por-id', motivoCancelacionController.getById);
+    this.router.put('/Actualizar-motivo-cancelacion', motivoCancelacionController.update);
+
+
 
     // Ruta extra para probar funcionalidad inexistente en PR
     // Borrar cuando se integre PR de gestion de categorias
